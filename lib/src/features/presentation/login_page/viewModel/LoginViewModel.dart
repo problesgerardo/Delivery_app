458604c2c8811e180dfd4185887a3108396cbb5c
Gerardo Perez <problesgerardo@gmail.com>
2023-04-21 21:26:35 -0700

    SignIn Implementation
    Implementacion del Login del Frontend con el backend

---
+++ lib/src/features/presentation/login_page/viewModel/LoginViewModel.dart
@@ -0,0 +1,99 @@
+
+
+import 'package:delivery_app/src/base/apiService/AppError.dart';
+import 'package:delivery_app/src/base/constants/LocalStorageKeys.dart';
+import 'package:delivery_app/src/features/domain/useCases/Auth/SignInUseCase/SignInUseCase.dart';
+import 'package:delivery_app/src/features/domain/useCases/Auth/SignInUseCase/SignInUseCaseBodyParameters.dart';
+import 'package:delivery_app/src/features/domain/useCases/LocalStorage/LocalStorageUseCaseParameters.dart';
+import 'package:delivery_app/src/features/domain/useCases/LocalStorage/SaveLocalStorageUseCase.dart';
+import 'package:delivery_app/src/features/presentation/login_page/model/LoginModel.dart';
+import 'package:delivery_app/src/features/presentation/state_providers/LoadingStateProvider.dart';
+import 'package:delivery_app/src/features/presentation/widgets/textFormField/CustomTextFormField.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../utils/helpers/ResultTypes/ResultType.dart';
+
+abstract class LoginViewModelInput {
+
+  late GlobalKey<FormState> formKey = GlobalKey<FormState>();
+  LoginModel? loginModel = LoginModel(email: '', password: '');
+  late LoadingStateProvider loadingStatusState;
+
+  void initState({ required LoadingStateProvider LoadingState });
+  Future<Result<bool, Failure>> login({ required String email, required String password });
+  bool isFormValidate();
+
+}
+abstract class LoginViewModel extends LoginViewModelInput with TextFormFieldDelegate{
+
+}
+
+class DefaultLoginViewModel extends LoginViewModel {
+
+   //Dependencias
+  final SignInUseCase _signInUseCase;
+  final SaveLocalStorageUseCase _saveLocalStorageUseCase;
+
+  DefaultLoginViewModel({ SignInUseCase? signInUseCase, SaveLocalStorageUseCase? saveLocalStorageUseCase })
+    : _signInUseCase = signInUseCase ?? DefaultSignInUseCase(),
+    _saveLocalStorageUseCase = saveLocalStorageUseCase ?? DefaultSaveLocalStorageUseCase();
+
+  @override
+  void initState({required LoadingStateProvider LoadingState}) {
+    loadingStatusState = LoadingState;
+  }
+
+  @override
+  bool isFormValidate() {
+    return formKey.currentState?.validate() ?? false;
+  }
+
+  @override
+  Future<Result<bool, Failure>> login({required String email, required String password}) {
+
+    loadingStatusState.setLoadingState(isLoading: true);
+
+    return _signInUseCase.execute(params: SignInUseCaseBodyParameters(email: email, password: password)).then((result){
+      switch(result.status) {
+        
+        case ResultStatus.success:
+          loadingStatusState.setLoadingState(isLoading: false);
+          _saveLocalStorageUseCase.execute(
+            parameters: SaveLocalStorageParameters(
+              key: LocalStorageKeys.idToken, 
+              value: result.value?.idToken ?? "",
+            )
+          );
+
+          return Result.success(true);
+
+        case ResultStatus.error:
+          loadingStatusState.setLoadingState(isLoading: false);
+          return Result.failure(result.error);
+      }
+    });
+
+  }
+
+  @override
+  onChange({required String newValue, required CustomTextFormFieldType customTextFormFieldType}) {
+    switch(customTextFormFieldType) {
+      
+      case CustomTextFormFieldType.email:
+        loginModel?.email = newValue;
+        break;
+      case CustomTextFormFieldType.password:
+        loginModel?.password = newValue;
+        break;
+      case CustomTextFormFieldType.phone:
+        // TODO: Handle this case.
+        break;
+      case CustomTextFormFieldType.username:
+        // TODO: Handle this case.
+        break;
+      case CustomTextFormFieldType.dateOfBirth:
+        // TODO: Handle this case.
+        break;
+    }
+  }
+}
